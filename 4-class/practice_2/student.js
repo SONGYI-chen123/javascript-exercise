d09d66991346c06673e11d8b1f9c6d9cf2dcd42f
SONGYI-chen123 <song1577660501@163.com>
2020-09-21 22:08:49 +0800

    add class student

--- 4-class/practice_2/student.js
+++ 4-class/practice_2/student.js
@@ -1,1 +1,13 @@
 // TODO 11: 在这里写实现代码
+export default class Student  {
+  constructor(name, age, klass) {
+    this.name = name;
+    this.age = age;
+    this.klass = klass;
+  }
+
+  introduce() {
+    const result = `I am a Student. I am at Class ${this.klass}.`;
+    return result;
+  }
+}
