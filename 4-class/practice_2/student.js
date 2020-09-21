1790b0eef75e8e2b7561b23708008c3a4db261dc
SONGYI-chen123 <song1577660501@163.com>
2020-09-21 22:15:30 +0800

    add class worker

--- 4-class/practice_2/student.js
+++ 4-class/practice_2/student.js
@@ -1,8 +1,8 @@
 // TODO 11: 在这里写实现代码
-export default class Student  {
+import Person from './person';
+export default class Student extends Person {
   constructor(name, age, klass) {
-    this.name = name;
-    this.age = age;
+    super(name,age);
     this.klass = klass;
   }
 
--- 4-class/practice_3/worker.js
+++ 4-class/practice_3/worker.js
@@ -1,1 +1,12 @@
 // TODO 14: 在这里写实现代码
+import Person from './person';
+export default class Person {
+    constructor(name, age) {
+      super(name,age);
+    }
+  
+    static introduce() {
+      const IntroduceString = `I am a Worker. I have a job.`;
+      return IntroduceString;
+    }
+}
