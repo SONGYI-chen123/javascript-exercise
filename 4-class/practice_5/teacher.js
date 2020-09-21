de4227e300726e9f7f3b2f597665a50842d0ae90
SONGYI-chen123 <song1577660501@163.com>
2020-09-21 22:26:27 +0800

    add class teacher

--- 4-class/practice_5/teacher.js
+++ 4-class/practice_5/teacher.js
@@ -1,1 +1,19 @@
 // TODO 20: 在这里写实现代码
+import Person from './person';
+export default class Student extends Person {
+  constructor(name, age, klass) {
+    super(name,age);
+    this.klass = klass;
+  }
+
+  introduce() {
+      let IntroduceString;
+      if(this.klass === undefined){
+        IntroduceString = `'My name is ${this.name}. I am ${this.age} years old. I am a Teacher. I teach No Class}.`;
+      }else{
+        IntroduceString = `'My name is ${this.name}. I am ${this.age} years old. I am a Teacher. I teach Class  ${this.klass}.`;
+      }
+   
+    return IntroduceString;
+  }
+}
