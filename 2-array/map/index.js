0e68f3014cac9fab85fa156c92391dcefa333ca3
SONGYI-chen123 <song1577660501@163.com>
2020-09-21 21:12:05 +0800

    map finished

--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,7 @@
 export default function numberMapToWord(collection) {
   // TODO 3: 在这里写实现代码
+  let transformCollection = collection.map((element) =>{
+      let transformChars = char(element+96); 
+  });
+  return transformCollection;
 }
