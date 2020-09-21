e4324fcbcf498526b4cd11104950d735770ef262
SONGYI-chen123 <song1577660501@163.com>
2020-09-21 20:47:57 +0800

    find finished

--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,3 +1,7 @@
 export default function find00OldPerson(collection) {
   // TODO 4: 在这里写实现代码
+  let findPerson = collection.find((element) => {
+    element.age >= 10 && element.age <= 20;
+  });
+  return findPerson.name;
 }
