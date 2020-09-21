fe55b49dee0697e136d4b2028ea8a9e55c41b82a
SONGYI-chen123 <song1577660501@163.com>
2020-09-21 20:35:26 +0800

    filter finished

--- 1-string/startsWith/index.js
+++ 1-string/startsWith/index.js
@@ -1,3 +1,11 @@
 export default function collectCarNumberCount(collection) {
   // TODO 7: 在这里写实现代码
+  let num = 0;
+  for( let i = 0;i<collection.length ;i += 1){
+    let subCollection = collection[i].subString(0,1);
+    if(subCollection === '粤A'){
+      num += 1;
+    }
+  }
+  return num;
 }
--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,15 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+    let newCollection = collection.filter( function(x){ return x % 3 === 0;} );
+    return newCollection;
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+   let noRepeatCollection = collection.filter( function(element,index,self){
+     return self.indexOf(element) === index;
+   });
+   return noRepeatCollection;
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
